51f8b4f22a332ce4e76a8e005eeb019c61fb728f
Chris Garrett <cgarrett@linkedin.com>
2019-11-21 17:21:09 -0800

    [BUGFIX] Adds additional base classes
    Adds Application, Router, and Resolver base classes, along with all of
    the Ember Data base classes. Also excepts Application and Router from
    the `reopenClass` errors, since Ember itself uses that method on them.

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -23,6 +23,10 @@ module.exports = function(environment) {
     }
   };
 
+  ENV['ember-cli-mirage'] = {
+    enabled: true
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
