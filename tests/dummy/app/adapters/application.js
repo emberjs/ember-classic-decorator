866b8e844046c2f6a69679a50d7ce88eee14effc
Chris Manson <chris@manson.ie>
2025-03-21 12:50:45 +0000

    remove mirage

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -2,4 +2,8 @@ import RESTAdapter from '@ember-data/adapter/rest';
 
 export default class MyRESTAdapter extends RESTAdapter {
   namespace = 'api';
+  buildURL() {
+    let url = super.buildURL(...arguments);
+    return `${url}.json`;
+  }
 }
